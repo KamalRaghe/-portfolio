dd819805c4b941b70c1807fc051ed921666d3b65
Kamal <kamal.raghe33@gmail.com>
2025-01-13 17:10:19 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div>
       <div className="center column" style={{backgroundColor:"cyan",height:"100vh"}} >
-        <div className="name" style={{color:"navy",position:"relative",right:"30px"}} >
+        <div className="first name" style={{color:"navy",position:"relative",right:"30px"}} >
           Kamal
         </div>
-        <div className="name" style={{color:"orange",position:"relative",left:"30px",bottom:"40px"}} >Raghe</div>
+        <div className="last name" style={{color:"orange",position:"relative",left:"30px",bottom:"40px"}} >Raghe</div>
         <div className="name" style={{backgroundColor:"purple",color:"white",position:"relative",bottom:"38px",fontSize:"40px",border:"1px solid purple",padding:"10px 70px"}}  >E-portfolio </div>
       </div>
     </div>
