6895fedb357910412e49246a6cbcfd2b57298e79
Kamal <kamal.raghe33@gmail.com>
2025-01-13 16:16:36 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <div className="center column" >
-      <div className="name" style={{color:"navy"}} >
-        <span style={{fontSize:"150px"}} >K</span>amal
+      <div className="name" style={{color:"navy",position:"relative",right:"30px"}} >
+        Kamal
       </div>
-      <div className="" >Raghe</div>
+      <div className="name" style={{color:"orange",position:"relative",left:"30px",bottom:"40px"}} >Raghe</div>
     </div>
   );
 }
