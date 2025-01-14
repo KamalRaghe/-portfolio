0e754b11814ae6e67a454d2df820911c567c68f8
Kamal <kamal.raghe33@gmail.com>
2025-01-13 19:19:35 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,12 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
    const [loaded, setLoaded] = useState(false)
-
+   const [none,setNone] = useState('')
    useEffect(()=>{
+    setTimeout(() => {
+      setLoaded(true)
+      setNone('none')
+    }, 2000);
     setTimeout(() => {
       setLoaded(true)
     }, 4000);
@@ -17,7 +21,10 @@ export default function Home() {
         <div className="last name" style={{color:"orange",position:"relative",left:"30px",bottom:"40px"}} >Raghe</div>
         <div className=" title name" style={{backgroundColor:"purple",color:"white",position:"relative",bottom:"38px",fontSize:"40px",border:"1px solid purple",padding:"10px 70px"}}  >
           E-portfolio 
-        </div></div>:<div className="section center column name" >Welcome</div>}
+        </div></div>:<div className="section center column name" >
+          <div className="intro" style={{display:none}} >Welcome</div>
+          <div className="to">to</div>
+        </div>}
       </div>
       <div className="center column section" style={{backgroundColor:"yellow"}} >
         <div className="first name" style={{color:"navy",position:"relative",right:"30px"}} >
