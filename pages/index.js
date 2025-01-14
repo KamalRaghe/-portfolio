0f5a385ae8c9caf47f15c48f1dab6762a04ddde6
Kamal <kamal.raghe33@gmail.com>
2025-01-14 15:20:19 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,22 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
    const [loaded, setLoaded] = useState(false)
-   
+
    useEffect(()=>{
     setTimeout(() => {
       setLoaded(true)
-    }, 4000);
+    }, 2000);
    },[])
   return (
     <div>
       <div className="center column section" style={{backgroundColor:"cyan"}} >
-      {loaded ? <div className="center column" ><div className="first name" style={{color:"navy",position:"relative",right:"30px"}} >
+      <div className="first name" style={{color:"navy",position:"relative",right:"30px"}} >
           Kamal
         </div>
         <div className="last name" style={{color:"orange",position:"relative",left:"30px",bottom:"40px"}} >Raghe</div>
-        <div className=" title name" style={{backgroundColor:"green",color:"white",position:"relative",bottom:"38px",fontSize:"40px",padding:"10px 70px"}}  >
+        <div className=" title name" style={{backgroundColor:"purple",color:"white",position:"relative",bottom:"38px",fontSize:"40px",padding:"10px 70px"}}  >
           E-portfolio 
-        </div></div>:<div className=" section center intro name">Welcome</div>}
-      </div>
-      <div className="center column section" style={{backgroundColor:"yellow"}} >
-        <div className="first name" style={{color:"navy",position:"relative",right:"30px"}} >
-          Kamal
         </div>
-        <div className="last name" style={{color:"orange",position:"relative",left:"30px",bottom:"40px"}} >Raghe</div>
-        <div className=" title name" style={{backgroundColor:"purple",color:"white",position:"relative",bottom:"38px",fontSize:"40px",border:"1px solid purple",padding:"10px 70px"}}  >E-portfolio </div>
       </div>
     </div>
   );
