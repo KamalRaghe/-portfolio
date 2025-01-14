1c7e87cda813475e21c3c44b12dedc6a2e53e627
Kamal <kamal.raghe33@gmail.com>
2025-01-13 20:18:26 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
    const [loaded, setLoaded] = useState(false)
-   const[none,setNone] = useState('')
-   const[none2,setNone2] = useState('none')
+   
    useEffect(()=>{
     setTimeout(() => {
       setLoaded(true)
@@ -12,15 +11,13 @@ export default function Home() {
   return (
     <div>
       <div className="center column section" style={{backgroundColor:"cyan"}} >
-      <div className="intro name" style={{display:none}} >Welcome</div>
-      <div className="to name" style={{display:none2}}>to</div>
-        {loaded && <div className="center column" ><div className="first name" style={{color:"navy",position:"relative",right:"30px"}} >
+      {loaded ? <div className="center column" ><div className="first name" style={{color:"navy",position:"relative",right:"30px"}} >
           Kamal
         </div>
         <div className="last name" style={{color:"orange",position:"relative",left:"30px",bottom:"40px"}} >Raghe</div>
-        <div className=" title name" style={{backgroundColor:"purple",color:"white",position:"relative",bottom:"38px",fontSize:"40px",border:"1px solid purple",padding:"10px 70px"}}  >
+        <div className=" title name" style={{backgroundColor:"green",color:"white",position:"relative",bottom:"38px",fontSize:"40px",padding:"10px 70px"}}  >
           E-portfolio 
-        </div></div>}
+        </div></div>:<div className=" section center intro name">Welcome</div>}
       </div>
       <div className="center column section" style={{backgroundColor:"yellow"}} >
         <div className="first name" style={{color:"navy",position:"relative",right:"30px"}} >
