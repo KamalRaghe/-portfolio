ce1f30d763707282570c73d7f9fb5b45dc98fc62
Kamal <kamal.raghe33@gmail.com>
2025-01-15 14:29:48 -0500

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }){
+  const router = useRouter();
   return <div>
-    <div className=" center" style={{height:'70px',width:"100%",position:"fixed",backgroundColor:"purple",color:"white",zIndex:"100",top:"0px"}} >Hello</div>
+    <div className=" center" style={{justifyContent:"end",height:'50px',width:"100vw",position:"fixed",backgroundColor:"purple",color:"white",zIndex:"100",top:"0px",paddingRight:"10px"}}>
+      <div style={{margin:"15px"}} onClick={()=>{router.push('/')}} >Home</div>
+      <div style={{margin:"15px"}} onClick={()=>{router.push('/')}} >About</div>
+      <div style={{margin:"15px"}} onClick={()=>{router.push('/')}} >Languages</div>
+      <div style={{margin:"15px"}} onClick={()=>{router.push('/')}} >Project</div>
+    </div>
     <Component {...pageProps} />
   </div>;
 }
--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,11 @@ export default function Home() {
         <div className=" title name" style={{backgroundColor:"purple",color:"white",position:"relative",bottom:"38px",fontSize:"40px",padding:"10px 70px"}}  >
           E-portfolio 
         </div>
+        <div className="center subtitle " style={{fontSize:"20px",color:"purple",position:"relative",bottom:"15px"}} >
+          <div style={{borderBottom:"2px solid purple"}} >About</div>
+          <div style={{margin:"0 30px",borderBottom:"2px solid purple"}} >Languages</div>
+          <div style={{borderBottom:"2px solid purple"}} >Project</div>
+        </div>
       </div>
     </div>
   );
