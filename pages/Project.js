bb58b4314a25ffc855227813ca67db109b21b418
Kamal <kamal.raghe33@gmail.com>
2025-01-16 15:40:27 -0500

    commit

--- pages/Project.js
+++ pages/Project.js
@@ -1,12 +1,10 @@
 export default function Languages(){
     return (
         <div className="center column section" style={{backgroundColor:"lime",fontSize:"25px",fontWeight:"bolder"}} >
-            <div className="fade" style={{width:"300px",padding:"2px",margin:"10px",textAlign:"center",borderBottom:"2px solid black"}}>
+            <div className="fade" style={{width:"220px",padding:"2px",margin:"10px",textAlign:"center",borderBottom:"2px solid black"}}>
                 This is my projects
             </div>
-            <div className="center fade" style={{backgroundColor:"white",width:"150px"}} >
-                
-            </div>
+            <
         </div>
     )
 }
