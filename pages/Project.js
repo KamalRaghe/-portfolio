577a2e8fa5f214e96cc3e8ca32dbc4d652e5a54e
Kamal <kamal.raghe33@gmail.com>
2025-01-16 15:41:25 -0500

    commit

--- pages/Project.js
+++ pages/Project.js
@@ -4,7 +4,17 @@ export default function Languages(){
             <div className="fade" style={{width:"220px",padding:"2px",margin:"10px",textAlign:"center",borderBottom:"2px solid black"}}>
                 This is my projects
             </div>
-            <
+            <div className="center" >
+                <div className="center fade" style={{backgroundColor:"white",width:"250px",height:"200px"}} >
+                    Project 1
+                </div>
+                <div className="center fade" style={{backgroundColor:"white",width:"250px",height:"200px",margin:"2npm0px"}} >
+                    Project 2
+                </div>
+                <div className="center fade" style={{backgroundColor:"white",width:"250px",height:"200px"}} >
+                    Project 3
+                </div>
+                </div>
         </div>
     )
 }
