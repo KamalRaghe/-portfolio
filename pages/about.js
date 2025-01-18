7fb02efee2fd04a0d635e3041b22d8f61d408266
Kamal <kamal.raghe33@gmail.com>
2025-01-18 10:25:53 -0500

    commit

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-export default function About(){
-    return (
-        <div className="center column section" style={{fontSize:"40px",fontWeight:"bolder"}} >
-            <div className="fade" style={{width:"330px",textAlign:"center"}}>
-                I am a self-employed frontend web developer
-            </div>
-        </div>
-    )
-}
