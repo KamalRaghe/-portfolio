c5ff33c0e571e7e41d832747fff832e8ce7e6799
Kamal <kamal.raghe33@gmail.com>
2025-01-18 10:27:59 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -50,6 +50,22 @@ export default function Home() {
                 <img style={{margin:"15px",width:"130px"}} src={'/js.png'}></img>
             </div>
         </div>
+        <div className="center column section" style={{fontSize:"25px",fontWeight:"bolder"}} >
+            <div className="fade" style={{width:"220px",padding:"2px",margin:"10px",textAlign:"center",borderBottom:"2px solid black"}}>
+                This is my projects
+            </div>
+            <div className="center" >
+                <div className="center fade" style={{backgroundColor:"white",width:"250px",height:"200px"}} >
+                    Project 1
+                </div>
+                <div className="center fade" style={{backgroundColor:"white",width:"250px",height:"200px",margin:"20px"}} >
+                    Project 2
+                </div>
+                <div className="center fade" style={{backgroundColor:"white",width:"250px",height:"200px"}} >
+                    Project 3
+                </div>
+                </div>
+        </div>
     </div>
   );
 }
