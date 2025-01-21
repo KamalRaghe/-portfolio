547f69df58978f41264b2ea6d913c598ff8be2cf
Kamal <kamal.raghe33@gmail.com>
2025-01-21 18:15:43 -0500

    commit

--- pages/Project.js
+++ pages/Project.js
@@ -4,10 +4,10 @@ export default function Home() {
   const router = useRouter()
   return (
     <div className="center column section" style={{fontSize:"25px",fontWeight:"bolder"}} >
+        <div className="center column" >
         <div  className="fade" style={{width:"160px",padding:"2px",margin:"20px",textAlign:"center",borderBottom:"2px solid white",paddingTop:"210px"}}>
             My projects
         </div>
-        <div className="center column" >
             <div className="center fade" style={{backgroundColor:"white",width:"250px",height:"200px"}} >
                 Project 1
             </div>
