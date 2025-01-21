a1c354771533ef48b70e8cb94e2f1f7e279f5559
Kamal <kamal.raghe33@gmail.com>
2025-01-21 17:43:29 -0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,24 @@ export default function Home() {
   const router = useRouter()
   return (
     <div>
-       <div className=" center" style={{cursor:"pointer",justifyContent:"end",height:'50px',width:"100vw",position:"fixed",zIndex:"100",top:"0px",paddingRight:"10px",color:"blue",borderTop:"0px",backgroundColor:"white"}}>
+      <div className=" center" style={{cursor:"pointer",justifyContent:"end",height:'50px',width:"100vw",position:"fixed",zIndex:"100",top:"0px",paddingRight:"10px",color:"white",borderTop:"0px",backgroundColor:"purple"}}>
       <div style={{margin:"15px"}} onClick={()=>{router.push('/')}} >Home</div>
       <div style={{margin:"15px"}}  onClick={()=>{router.push('/About')}} >About</div>
       <div style={{margin:"15px"}}  onClick={()=>{router.push('/Languages')}} >Languages</div>
       <div style={{margin:"15px"}}  onClick={()=>{router.push('/Project')}} >Project</div>
     </div>
-      <div ref = {home} className="center column section">
-      <div className="first name" style={{color:"navy",position:"relative",right:"30px"}} >
+      <div className="center column section">
+      <div className="first name" style={{color:"blue",position:"relative",right:"30px"}} >
           Kamal
         </div>
         <div className="last name" style={{color:"orange",position:"relative",left:"30px",bottom:"40px"}} >Raghe</div>
         <div className=" title name" style={{backgroundColor:"purple",color:"white",position:"relative",bottom:"38px",fontSize:"40px",padding:"10px 70px"}}  >
           E-portfolio 
         </div>
-        <div className="center subtitle " style={{fontSize:"20px",color:"blue",position:"relative",bottom:"15px",cursor:"pointer"}} >
-          <div style={{borderBottom:"2px solid blue"}} onClick={()=>{router.push('/About')}} >About</div>
-          <div style={{margin:"0 30px",borderBottom:"2px solid blue"}} onClick={()=>{router.push('/Languages')}} >Languages</div>
-          <div style={{borderBottom:"2px solid blue"}} onClick={()=>{router.push('/Project')}}  >Project</div>
+        <div className="center subtitle " style={{fontSize:"20px",color:"purple",position:"relative",bottom:"15px",cursor:"pointer"}} >
+          <div style={{borderBottom:"2px solid purple"}} onClick={()=>{router.push('/About')}} >About</div>
+          <div style={{margin:"0 30px",borderBottom:"2px solid purple"}} onClick={()=>{router.push('/Languages')}} >Languages</div>
+          <div style={{borderBottom:"2px solid purple"}} onClick={()=>{router.push('/Project')}}  >Project</div>
         </div>
       </div>
       <div className="center column section" style={{fontSize:"40px",fontWeight:"bolder"}} >
