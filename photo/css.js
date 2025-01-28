96988a98b5f919e94435f0ba0309e51a1c6aeeb1
Kamal <kamal.raghe33@gmail.com>
2025-01-28 16:10:19 -0500

    commit

---
+++ photo/css.js
@@ -0,0 +1,5 @@
+export default function HtmlPic(){
+    return <>
+        <img style={{margin:"15px",width:"130px",scale:"1.65",position:"relative",bottom:"27px"}} src={'/html.png'}></img>
+    </>
+}
