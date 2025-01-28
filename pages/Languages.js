37f23b217682ea1bad80854a27f6d415d5dafed7
Kamal <kamal.raghe33@gmail.com>
2025-01-28 11:39:46 -0500

    commit

--- pages/Languages.js
+++ pages/Languages.js
@@ -10,17 +10,17 @@ export default function Home() {
                     My technology stack
                 </div>
                     <div className="screen" style={{position:"relative"}} >
-                    <div><img style={{margin:"15px",width:"130px",scale:"1.65",position:"relative",bottom:"27px"}} src={'/html.png'}></img></div>
+                    <div></div><img style={{margin:"15px",width:"130px",scale:"1.65",position:"relative",bottom:"27px"}} src={'/html.png'}></img>
                     <img style={{margin:"15px",width:"130px"}} src={'/css.png'}></img>
-                    <img style={{margin:"15px",width:"130px"}} src={'/js.png'}></img>
-                    <img style={{margin:"15px",width:"130px"}} src={'/js.png'}></img>
+                    <img style={{margin:"15px",width:"130px",scale:"1.65",position:"relative",bottom:"27px"}} src={'/js.png'}></img>
+                    <img style={{margin:"15px",width:"130px",position:"relative",bottom:"27px"}} src={'/react.png'}></img>
                     <img style={{margin:"15px",width:"130px"}} src={'/js.png'}></img>
                     <img style={{margin:"15px",width:"130px"}} src={'/js.png'}></img>
                     <img style={{margin:"15px",width:"130px"}} src={'/js.png'}></img>
                     <img style={{margin:"15px",width:"130px"}} src={'/js.png'}></img>
                     </div>
                 </div>
-            </div>
+            </div>scale:"1.65",position:"relative",bottom:"27px"
         </div>
   );
 }
