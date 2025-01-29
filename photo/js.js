d79b2fd91de0021b51f467e9a02f3feaf00b7d57
Kamal <kamal.raghe33@gmail.com>
2025-01-28 20:52:18 -0500

    commit

--- photo/js.js
+++ photo/js.js
@@ -1,6 +1,6 @@
 export default function JsPic(){
     return <span>
-        <span style={{position:"relative",bottom:"166px",left:"82px"}} >Javascript</span>
+        <span style={{position:"relative",bottom:"166px",left:"52px"}} >Javascript</span>
         <img style={{width:"130px",position:"relative",bottom:"13px",right:"70px",scale:"1.65",position:"relative",bottom:"40px"}} src={'/js.png'}></img>
     </span>
 }
