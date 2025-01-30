2802878ca20d223324041ab90c2e5145ef3153ff
Kamal <kamal.raghe33@gmail.com>
2025-01-29 19:59:30 -0500

    commit

--- photo/react.js
+++ photo/react.js
@@ -1,5 +1,5 @@
 export default function ReactPic(){
-    return <span className="center column" style={{width:"160px",border:"1px solid white",position:"relative",left:"67px"}} >
+    return <span className="center column" style={{width:"160px",border:"1px solid white",position:"relative"}} >
         <span> React </span>
         <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px"}} src={'/react.png'}></img>
     </span>
