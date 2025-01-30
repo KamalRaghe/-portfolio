b8afdfae010be5ac98b9a4ca9c70779a90885b40
Kamal <kamal.raghe33@gmail.com>
2025-01-29 20:03:38 -0500

    commit

--- photo/css.js
+++ photo/css.js
@@ -1,6 +1,6 @@
 export default function CssPic(){
-    return <span>
-        <span style={{position:"relative",bottom:"166px",left:"90px"}} >Css</span>
-        <img style={{width:"130px",position:"relative",bottom:"13px",left:"8px"}} src={'/css.png'}></img>
-    </span>
+    return <span className="center column" style={{width:"170px",position:"relative",right:"10px"}} >
+    <span> React </span>
+    <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px"}} src={'/react.png'}></img>
+</span>
 }
--- photo/html.js
+++ photo/html.js
@@ -1,6 +1,6 @@
 export default function HtmlPic(){
     return <span className="center column" style={{width:"170px",position:"relative",right:"10px"}} >
-    <   span> React </span>
-        <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px"}} src={'/react.png'}></img>
+    <   span>Html</span>
+        <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px"}} src={'/html.png'}></img>
     </span>
 }
