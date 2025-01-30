decdc26abcedb1f9c953a3fd799e606d512ca728
Kamal <kamal.raghe33@gmail.com>
2025-01-29 20:16:05 -0500

    commit

--- pic.js
+++ pic.js
@@ -1,6 +1,10 @@
-export default function Pic({title,pic}){
+import { useState } from "react"
+
+export default function Pic({title,pic,small}){
+    const [bigger,setBigger] = useState('1')
+    
     return <span className="center column" style={{width:"170px",position:"relative",right:"10px"}} >
         <span>{title}</span>
-        <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px"}} src={pic}></img>
+        <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
     </span>
 }
