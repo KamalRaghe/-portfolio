656781739fa6d62e8ca9312b8ee9cbca5f23966e
Kamal <kamal.raghe33@gmail.com>
2025-01-29 20:04:14 -0500

    commit

--- photo/css.js
+++ photo/css.js
@@ -1,6 +1,6 @@
 export default function CssPic(){
     return <span className="center column" style={{width:"170px",position:"relative",right:"10px"}} >
-    <span> React </span>
-    <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px"}} src={'/react.png'}></img>
+    <span> Css </span>
+    <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px"}} src={'/css.png'}></img>
 </span>
 }
