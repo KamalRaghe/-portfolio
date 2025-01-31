2413d9b651c8562b4d59f6bb8477543acde60cef
Kamal <kamal.raghe33@gmail.com>
2025-01-31 18:17:28 -0500

    commit

--- pages/Languages.js
+++ pages/Languages.js
@@ -18,22 +18,30 @@ export default function Home() {
       <div className="center column" style={{fontSize:"40px",fontWeight:"bolder"}} >
             <div className="center column" style={{fontSize:"25px",fontWeight:"bolder"}} >
                 <div className="center column fade" style={{paddingTop:"90px"}} >
-                <div style={{width:"250px",padding:"2px",marginBottom:"50px",textAlign:"center",borderBottom:"2px solid white"}}>
+                <div style={{width:"250px",padding:"2px",marginBottom:"50px",textAlign:"center"}}>
                     My technology stack
                 </div>
                     <div style={{position:"relative",border:"2px solid white"}} >
                     <div className="center screen" style={{justifyContent:'start',marginBottom:"10px"}} >
-                        {time+800 < Date.now() && <Pic title = {"Html"} pic ={'html.png'} small={true} top={'40px'}></Pic>}
-                        {time+1600 < Date.now() && <Pic title = {"Css"} pic ={'css.png'} top={'10px'}></Pic>}
+                        <div className="center" >
+                            {time+800 < Date.now() && <Pic title = {"Html"} pic ={'html.png'} small={true} top={'40px'}></Pic>}
+                            {time+1600 < Date.now() && <Pic title = {"Css"} pic ={'css.png'} top={'10px'}></Pic>}
+                        </div>
                        <div className="center" >
                             {time+2400 < Date.now() && <Pic title = {"JavaScript"} pic ={'js.png'} top={'38px'} small={true} ></Pic>}
                             {time+3200 < Date.now() && <Pic title = {"TypeScript"} pic ={'ts.png'} top={'10px'}></Pic>}
                        </div>
                     </div>
-                    <div className="center" style={{justifyContent:'start'}} >
-                        {time+4000 < Date.now() && <Pic title = {"React"} pic ={'react.png'} top={'20px'}></Pic>}
-                        {time+4800 < Date.now() && <Pic title = {"Next"} pic ={'next.png'} top={'13px'} ></Pic>}
-                        {time+5600 < Date.now() && <Pic title = {"Redux"} pic ={'redux.png'} top={'15px'}></Pic>}
+                    <div className="center screen" style={{justifyContent:'start'}} >
+                        <div className="center" >
+                            {time+4000 < Date.now() && <Pic title = {"React"} pic ={'react.png'} top={'20px'}></Pic>}
+                            {time+4800 < Date.now() && <Pic title = {"Next"} pic ={'next.png'} top={'13px'} ></Pic>}
+                        </div>
+                        <div className="center" >
+                            {time+5600 < Date.now() && <Pic title = {"Redux"} pic ={'redux.png'} top={'15px'}></Pic>}
+                            {time+5600 < Date.now() && <div><Pic title = {""} pic ={''} top={'15px'}></Pic></div>}
+                        </div>
+                        
                     </div>
                     </div>
                 </div>
