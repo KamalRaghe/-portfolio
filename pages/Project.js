18a02ec8eab6596b510d5c9099e07b38b8700ec1
Kamal <kamal.raghe33@gmail.com>
2025-02-02 22:05:39 -0500

    commit

--- pages/Project.js
+++ pages/Project.js
@@ -9,13 +9,13 @@ export default function Home() {
         <div  className="fade" style={{width:"160px",padding:"2px",margin:"20px",textAlign:"center",borderBottom:"2px solid white",paddingTop:"50px"}}>
             My projects
         </div>
-            <div className="center fade" style={{scale:"2",margin:"40px",marginBottom:"90px"}} >
+            <div className="center fade" style={{scale:"2",margin:"40px",marginBottom:"130px"}} >
                 <Pic title = {""} pic ={'pic1.png'} top={'38px'} small={true} ></Pic>
             </div>
-            <div className="center fade" style={{scale:"2",margin:"20px",marginBottom:"90px"}} >
+            <div className="center fade" style={{scale:"2",margin:"20px",marginBottom:"150px"}} >
                 <Pic title = {""} pic ={'pic2.png'} top={'38px'} small={true} ></Pic>
             </div>
-            <div className="center fade" style={{scale:"2",marginBottom:"90px"}} >
+            <div className="center fade" style={{scale:"2",marginBottom:"150px"}} >
                 <Pic title = {""} pic ={'pic3.png'} top={'38px'} small={true} ></Pic>
             </div>
             </div>
