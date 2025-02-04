dabee07d9bdc53742f8ae359bb45cc9bfdaef46d
Kamal <kamal.raghe33@gmail.com>
2025-02-03 22:32:36 -0500

    commit

--- pages/Project.js
+++ pages/Project.js
@@ -9,14 +9,14 @@ export default function Home() {
         <div  className="fade" style={{width:"160px",padding:"2px",margin:"20px",textAlign:"center",borderBottom:"2px solid white",paddingTop:"50px"}}>
             My projects
         </div>
-            <div className="center fade glow" style={{scale:"2",margin:"40px",marginBottom:"130px",position:"relative",left:"29px"}} >
-                <Pic title = {""} pic ={'pic1.png'} top={'38px'} small={true} ></Pic>
+            <div className="center fade" style={{scale:"2",margin:"40px",marginBottom:"130px",position:"relative",left:"32px"}} >
+                <Pic glow={true} title = {""} pic ={'pic1.png'} top={'38px'} small={true} ></Pic>
             </div>
-            <div className="center fade glow" style={{scale:"2",margin:"20px",marginBottom:"150px",position:"relative",left:"29px"}} >
-                <Pic title = {""} pic ={'pic2.png'} top={'38px'} small={true} ></Pic>
+            <div className="center fade" style={{scale:"2",margin:"20px",marginBottom:"150px",position:"relative",left:"32px"}} >
+                <Pic glow={true} title = {""} pic ={'pic2.png'} top={'38px'} small={true} ></Pic>
             </div>
-            <div className="center fade glow" style={{scale:"2",marginBottom:"150px",position:"relative",left:"29px"}} >
-                <Pic title = {""} pic ={'pic3.png'} top={'38px'} small={true} ></Pic>
+            <div className="center fade" style={{scale:"2",marginBottom:"150px",position:"relative",left:"32px"}} >
+                <Pic glow={true} title = {""} pic ={'pic3.png'} top={'38px'} small={true} ></Pic>
             </div>
             </div>
     </div>
--- pic.js
+++ pic.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export default function Pic({title,pic,small,top}){
+export default function Pic({title,pic,small,top,glow}){
     const [bigger,setBigger] = useState('1')
     useEffect(()=>{
         if(small){
@@ -9,6 +9,7 @@ export default function Pic({title,pic,small,top}){
     })
     return <span className="center column fade" style={{width:"170px",position:"relative",right:"10px"}} >
         <span style={{position:"relative",bottom:top,marginTop:"35px"}} >{title}</span>
-        <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
+        {glow ? <img className='glow' style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
+        :<img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>}
     </span>
 }
