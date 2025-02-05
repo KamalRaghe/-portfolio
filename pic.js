6c1e6f2d53f53501ca38a0dd336c9c06a345f018
Kamal <kamal.raghe33@gmail.com>
2025-02-04 19:46:58 -0500

    commit

--- pic.js
+++ pic.js
@@ -7,9 +7,11 @@ export default function Pic({title,pic,small,top,glow}){
             setBigger('1.65')
         }
     })
-    return <span className="center column fade" style={{width:"170px",position:"relative",right:"10px"}} >
-        {glow ? <span style={{position:"relative",top:'55px',marginTop:"35px",zIndex:"10"}} >{title}</span> :<span style={{position:"relative",bottom:top,marginTop:"35px"}} >{title}</span>}
-        {glow ? <img className='glow' style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
-        :<img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>}
+    return <span className="center column fade" style={{position:"relative",right:"10px"}} >
+        <div className="glow" style={{width:"200px",height:"150px"}} >
+            {glow ? <span style={{position:"relative",top:'55px',marginTop:"35px",zIndex:"10"}} >{title}</span> :<span style={{position:"relative",bottom:top,marginTop:"35px"}} >{title}</span>}
+            {glow ? <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
+            :<img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>}
+        </div>
     </span>
 }
