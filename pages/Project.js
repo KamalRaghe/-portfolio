a56f85d42b166534f2dfc4baf792903b8763c7fd
Kamal <kamal.raghe33@gmail.com>
2025-02-04 19:15:01 -0500

    commit

--- pages/Project.js
+++ pages/Project.js
@@ -11,9 +11,11 @@ export default function Home() {
         </div>
             <div className="center fade" style={{scale:"2",margin:"40px",marginBottom:"130px",position:"relative",left:"32px"}} >
                 <Pic glow={true} title = {""} pic ={'pic1.png'} top={'38px'} small={true} ></Pic>
+                <div className="center" style={{position:"absolute",top:"60px",right:"10px",width:"170px"}} >Nft market</div>
             </div>
             <div className="center fade" style={{scale:"2",margin:"20px",marginBottom:"150px",position:"relative",left:"32px"}} >
                 <Pic glow={true} title = {""} pic ={'pic2.png'} top={'38px'} small={true} ></Pic>
+                <div className="center" style={{position:"absolute",top:"60px",right:"10px",width:"170px"}} >Book store</div>
             </div>
             <div className="center fade" style={{color:"purple",scale:"2",marginBottom:"150px",position:"relative",left:"32px"}} >
                 <Pic glow={true} title = {""} pic ={'pic3.png'} top={'38px'} small={true} ></Pic>
