5038ee34bb399f60ac52da1f92b1786eb3045a06
Kamal <kamal.raghe33@gmail.com>
2025-02-05 15:33:45 -0500

    commit

--- pic.js
+++ pic.js
@@ -9,8 +9,8 @@ export default function Pic({title,pic,small,top,glow,link}){
             setBigger('1.65')
         }
     })
-    return <span className="center column fade" style={{position:"relative",right:"10px"}} >
-        {glow ? <span className="click glow" onClick={()=>{router.push(link)}} style={{position:"relative",top:'55px',marginTop:"35px",zIndex:"10"}} >{title}</span> :<span style={{position:"relative",bottom:top,marginTop:"35px"}} >{title}</span>}
+    return <span className="center column fade" onClick={()=>{if(glow){router.push(link)}}} style={{position:"relative",right:"10px"}} >
+        {glow ? <span className="click glow" style={{position:"relative",top:'55px',marginTop:"35px",zIndex:"10"}} >{title}</span> :<span style={{position:"relative",bottom:top,marginTop:"35px"}} >{title}</span>}
         {glow ? <img className="glow" style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
         :<img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>}
     </span>
