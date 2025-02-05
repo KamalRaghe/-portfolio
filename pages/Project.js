ea86c60b9bab67500d7aabd2c35516a6ace2d3b1
Kamal <kamal.raghe33@gmail.com>
2025-02-04 19:43:38 -0500

    commit

--- pages/Project.js
+++ pages/Project.js
@@ -9,11 +9,10 @@ export default function Home() {
         <div  className="fade" style={{width:"160px",padding:"2px",margin:"20px",textAlign:"center",borderBottom:"2px solid white",paddingTop:"50px"}}>
             My projects
         </div>
-            <div className="center fade" style={{scale:"2",margin:"40px",marginBottom:"130px",position:"relative",left:"32px"}} >
-                <Pic glow={true} title = {""} pic ={'pic1.png'} top={'38px'} small={true} ></Pic>
-                <div className="center" style={{position:"absolute",top:"60px",right:"10px",width:"170px",color:"navy"}} >Nft market</div>
+            <div className="center fade link" style={{scale:"2",margin:"40px",marginBottom:"130px",position:"relative",left:"32px"}} >
+                <Pic glow={true} title = {"Nft market"} pic ={'pic1.png'} top={'38px'} small={true} ></Pic>
             </div>
-            <div className="center fade" style={{scale:"2",margin:"20px",marginBottom:"150px",position:"relative",color:"navy",left:"32px"}} >
+            <div className="center fade link" style={{scale:"2",margin:"20px",marginBottom:"150px",position:"relative",left:"32px"}} >
                 <Pic glow={true} title = {"Book store"} pic ={'pic2.png'} top={'38px'} small={true} ></Pic>
             </div>
             <div className="center fade link" style={{scale:"2",marginBottom:"150px",position:"relative",left:"32px"}} >
--- pic.js
+++ pic.js
@@ -2,14 +2,20 @@ import { useEffect, useState } from "react"
 
 export default function Pic({title,pic,small,top,glow}){
     const [bigger,setBigger] = useState('1')
+    const [Glow,setGlow] = useState('')
     useEffect(()=>{
         if(small){
             setBigger('1.65')
         }
+        if(glow){
+            setGlow('center column fade glow')
+        }else{
+            setGlow('center column fade')
+        }
     })
-    return <span className="center column link fade" style={{width:"170px",position:"relative",right:"10px"}} >
+    return <span className={Glow} style={{width:"170px",position:"relative",right:"10px"}} >
         {glow ? <span style={{position:"relative",top:'55px',marginTop:"35px",zIndex:"10"}} >{title}</span> :<span style={{position:"relative",bottom:top,marginTop:"35px"}} >{title}</span>}
-        {glow ? <img className='glow' style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
+        {glow ? <img  style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
         :<img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>}
     </span>
 }
