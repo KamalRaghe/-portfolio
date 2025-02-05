e599344aeb7d74fd95de9d5a574ea65cde60dafd
Kamal <kamal.raghe33@gmail.com>
2025-02-04 19:55:40 -0500

    commit

--- pic.js
+++ pic.js
@@ -1,14 +1,16 @@
+import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
 export default function Pic({title,pic,small,top,glow}){
     const [bigger,setBigger] = useState('1')
+    const router = useRouter()
     useEffect(()=>{
         if(small){
             setBigger('1.65')
         }
     })
     return <span className="center column fade" style={{position:"relative",right:"10px"}} >
-        {glow ? <span className="click glow" style={{position:"relative",top:'55px',marginTop:"35px",zIndex:"10"}} >{title}</span> :<span style={{position:"relative",bottom:top,marginTop:"35px"}} >{title}</span>}
+        {glow ? <span className="click glow" onClick={()=>{router.push(link)}} style={{position:"relative",top:'55px',marginTop:"35px",zIndex:"10"}} >{title}</span> :<span style={{position:"relative",bottom:top,marginTop:"35px"}} >{title}</span>}
         {glow ? <img className="glow" style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
         :<img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>}
     </span>
