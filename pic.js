18022098c1212c536e8fb7af904560707897331b
Kamal <kamal.raghe33@gmail.com>
2025-02-04 19:47:53 -0500

    commit

--- pic.js
+++ pic.js
@@ -8,10 +8,8 @@ export default function Pic({title,pic,small,top,glow}){
         }
     })
     return <span className="center column fade" style={{position:"relative",right:"10px"}} >
-        <div className="glow" style={{width:"200px",height:"150px"}} >
-            {glow ? <span style={{position:"relative",top:'55px',marginTop:"35px",zIndex:"10"}} >{title}</span> :<span style={{position:"relative",bottom:top,marginTop:"35px"}} >{title}</span>}
-            {glow ? <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
-            :<img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>}
-        </div>
+        {glow ? <span style={{position:"relative",top:'55px',marginTop:"35px",zIndex:"10"}} >{title}</span> :<span style={{position:"relative",bottom:top,marginTop:"35px"}} >{title}</span>}
+        {glow ? <img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>
+        :<img style={{margin:"15px",width:"130px",position:"relative",bottom:"5px",scale:bigger}} src={pic}></img>}
     </span>
 }
